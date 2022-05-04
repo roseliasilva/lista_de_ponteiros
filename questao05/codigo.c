d556973ea2707f59134aa8fc703f6740587d0fcc
Rosélia Silva <roselianascimento.1611@gmail.com>
2022-05-03 22:13:05 -0300

    resposta questao05

--- questao05/codigo.c
+++ questao05/codigo.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 
 int main(void){
-  float vet[5] = {1.1,2.2,3.3,4.4,5.5};
-  float *f;
-  int i;
-  f = vet;
+  
+  float vet[5] = {1.1,2.2,3.3,4.4,5.5}; //vetor de float
+  float *f; //ponteiro para float
+  int i; //inteiro para inicialização do for
+  f = vet; //f aponta para vet
   printf("contador/valor/valor/endereco/endereco");
+
+  //repetir 4 vezes
   for(i = 0 ; i <= 4 ; i++){
-  printf("\ni = %d",i);
-  printf("vet[%d] = %.1f",i, vet[i]);
-  printf("*(f + %d) = %.1f",i, *(f+i));
-  printf("&vet[%d] = %X",i, &vet[i]);
-  printf("(f + %d) = %X",i, f+i);
+    //imprimir o contador de 0 a 4
+    printf("\ni = %d; ",i); //esperado
+    //vet[i] = vet[i] -> valores
+    printf("vet[%d] = %.1f; ",i, vet[i]); //esperado
+    //mesma coisa do anterior
+    printf("*(f + %d) = %.1f; ",i, *(f+i));
+    //imprimir os endereços das posições
+    printf("&vet[%d] = %X; ",i, &vet[i]);
+    //mesma coisa do anterior
+    printf("(f + %d) = %X; ",i, f+i);
   }
 }
+
+//todos os valores foram esperados, porém
+//no 3° e no 5° printf foram usadas atribuições
+//diferentes
